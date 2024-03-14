140559b8d4b8909710e1b33537855106fc83a269
Boswellyannick <163435646+Boswellyannick@users.noreply.github.com>
2024-03-14 15:33:40 +0000

    Add files via upload

---
+++ new.js
@@ -0,0 +1,101 @@
+function previous() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content').scrollLeft -= widthSlider;
+
+}
+
+function next() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content').scrollLeft += widthSlider;
+}
+
+function previous1() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content1').scrollLeft -= widthSlider;
+
+}
+
+function next1() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content1').scrollLeft += widthSlider;
+}
+
+function previous2() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content2').scrollLeft -= widthSlider;
+
+}
+
+function next2() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content2').scrollLeft += widthSlider;
+}
+
+function previous3() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content3').scrollLeft -= widthSlider;
+
+}
+
+function next3() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content3').scrollLeft += widthSlider;
+}
+
+
+function previous4() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content4').scrollLeft -= widthSlider;
+
+}
+
+function next4() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content4').scrollLeft += widthSlider;
+}
+
+function previous5() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content5').scrollLeft -= widthSlider;
+
+}
+
+function next5() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content5').scrollLeft += widthSlider;
+}
+
+function previous6() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content6').scrollLeft -= widthSlider;
+
+}
+
+function next6() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content6').scrollLeft += widthSlider;
+}
+
+
+function previous7() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content7').scrollLeft -= widthSlider;
+
+}
+
+function next7() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content7').scrollLeft += widthSlider;
+}
+
+function previous8() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content8').scrollLeft -= widthSlider;
+
+}
+
+function next8() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content8').scrollLeft += widthSlider;
+}
+
---
+++ rio.js
@@ -0,0 +1,43 @@
+function previous() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content').scrollLeft -= widthSlider;
+
+}
+
+function next() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content').scrollLeft += widthSlider;
+}
+
+function previous1() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content1').scrollLeft -= widthSlider;
+
+}
+
+function next1() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content1').scrollLeft += widthSlider;
+}
+
+function previous2() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content2').scrollLeft -= widthSlider;
+
+}
+
+function next2() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content2').scrollLeft += widthSlider;
+}
+
+function previous3() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content3').scrollLeft -= widthSlider;
+
+}
+
+function next3() {
+    const widthSlider = document.querySelector('.left').offsetWidth;
+    document.querySelector('.image__content3').scrollLeft += widthSlider;
+}
